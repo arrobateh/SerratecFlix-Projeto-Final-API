c2a81bbddbddf9ddae7c19dbd65503409a8a2674
Bryanxrt <higorbraga24@gmail.com>
2026-05-22 18:28:20 -0300

    Spring com Dependencias

---
+++ SerratecFlix/src/main/java/org/serratec/SerratecFlix/SerratecFlixApplication.java
@@ -0,0 +1,13 @@
+package org.serratec.SerratecFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerratecFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerratecFlixApplication.class, args);
+	}
+
+}
